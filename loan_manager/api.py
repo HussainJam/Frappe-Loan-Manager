aef43cacd6415bf0cc40cad1647d820abf4aac4d
Jamal <jamal.se17@iba-suk.edu.pk>
2025-02-25 17:44:31 +0400

    loan manager app

--- loan_manager/api.py
+++ loan_manager/api.py
@@ -1,30 +1,19 @@
-# import frappe
-# from frappe import _
-
-# @frappe.whitelist()
-# def get_total_payable():
-#     total_amount = frappe.db.sql("""
-#         SELECT SUM(amount) 
-#         FROM `tabTransaction`
-#         WHERE docstatus = 1
-#     """, as_list=True)
-
-#     return {
-#         "value": total_amount[0][0] if total_amount and total_amount[0][0] else 0,
-#         "fieldtype": "Currency"
-#     }
-
-
 import frappe
+from frappe import _
+
 
 @frappe.whitelist()
-def get_total_payable():
-    total_amount = frappe.db.sql("""
-        SELECT SUM(amount) 
-        FROM `tabTransaction`
-    """, as_list=True)
+def get_paid_amount_for_logged_in_user():
+    user = frappe.session.user
+    value = calculate_paid_amount(user)  
 
     return {
-        "value": total_amount[0][0] if total_amount and total_amount[0][0] else 0,
-        "fieldtype": "Currency"
+        "value": value,
+        "fieldtype": "Currency",
+        "route_options": {"from_date": "2023-05-23"},
+        "route": ["query-report", "Permitted Documents For User"]
     }
+
+def calculate_paid_amount(user):
+    
+    return 800.00 
--- loan_manager/loan_manager/report/user_transactions_report/user_transactions_report.py
+++ loan_manager/loan_manager/report/user_transactions_report/user_transactions_report.py
@@ -1,45 +1,6 @@
-# import frappe
-# from frappe import _
+import frappe
+from frappe import _
 
-# def execute(filters=None):
-#     columns = [
-#         {"label": _("Name"), "fieldname": "name", "fieldtype": "Data", "width": 150},
-#         {"label": _("Date"), "fieldname": "date", "fieldtype": "Date", "width": 100},
-#         {"label": _("Description"), "fieldname": "description", "fieldtype": "Data", "width": 250},
-#         {"label": _("Amount"), "fieldname": "amount", "fieldtype": "Currency", "width": 100},
-#     ]
-    
-#     conditions = []
-#     if filters.get("from_date"):
-#         conditions.append("transaction_date >= %(from_date)s")
-#     if filters.get("to_date"):
-#         conditions.append("transaction_date <= %(to_date)s")
-#     if filters.get("user"):
-#         conditions.append("User = %(user)s")
-
-#     # Construct the SQL query
-#     transactions = frappe.db.sql("""
-#         SELECT 
-#             name,
-#             transaction_date AS date,
-#             description,
-#             amount 
-#         FROM 
-#             `tabTransactions` 
-#         WHERE 
-#             docstatus < 2
-#             {conditions}
-#         ORDER BY 
-#             transaction_date
-#     """.format(conditions=" AND ".join(conditions) if conditions else ""), 
-#     as_dict=1, 
-#     params={
-#         'from_date': filters.get("from_date"),
-#         'to_date': filters.get("to_date"),
-#         'user': filters.get("user")
-#     })
-
-#     return columns, transactions
 
 
 def formatter(value, row, column, data, default_formatter):
